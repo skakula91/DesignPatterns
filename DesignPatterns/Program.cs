9bdb23ba23c0a90728f5832816b3bf3b55415e69
Sai Akula <saikiranakula@hotmail.com>
2020-05-10 23:02:05 -0500

    singleton

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -4,14 +4,30 @@ using DesignPatterns.CompositePattern.Component;
 using DesignPatterns.DecoratorPattern.Component;
 using DesignPatterns.DecoratorPattern.ConcreteComponent;
 using DesignPatterns.DecoratorPattern.ConcreteDecorator;
+using DesignPatterns.Singleton;
 using System;
+using System.Threading.Tasks;
 
 namespace DesignPatterns
 {
     class Program
     {
+        // Types of Design Patterns
+        //1. Creational: This type deals with the object creation and initialization. This pattern gives the 
+        // program more flexibiliy in deciding which objects need to be create for a given case
+        // ex: singleton, factory, abstract factory.. etc
+        //2. Structural: This type deals with class and object composition.
+        // This pattern focuses on decoupling interface and implementation of classes and its objects.
+        // ex: Adapter, Bridge.. etc
+        //3. Behavioural : deals with communication b/w objects and classes ex: command, interpreter.. etc
+
         static void Main(string[] args)
         {
+            // Creational Patterns
+            //1. Singleton
+            Parallel.Invoke(() => Test1Prnt(), () => Test2Prnt());
+
+            // Structural patterns
             //1. Composite Pattern - Hierarchy, folder structures and menu bars
             IDirectReport dr1 = new DirectReport("Report1", "IT");
             IDirectReport dr2 = new DirectReport("Report2", "IT");
@@ -21,7 +37,7 @@ namespace DesignPatterns
 
             IDirectReport mgr1 = new Manager("ManagerIT", "IT")
             {
-                DirectReports = {dr1,dr2,dr3}
+                DirectReports = { dr1, dr2, dr3 }
             };
             IDirectReport mgr2 = new Manager("ManagerHR", "HR")
             {
@@ -44,9 +60,21 @@ namespace DesignPatterns
             //3. Adapter Pattern - Combine two incompatibe classes and interfaces using adapter pattern
             //Diff b/w decorator and adapter https://stackoverflow.com/questions/17731522/what-are-the-differences-between-the-decorator-and-adapter-patterns/17731655
             IEmployee emp = new EmployeeAdapter();
-            var jsonData = emp.GetEmployees();
-            Console.WriteLine(jsonData);
+            //  var jsonData = emp.GetEmployees();
+            //Console.WriteLine(jsonData);
             Console.ReadLine();
+        }
+
+        private static void Test2Prnt()
+        {
+            SingletonExample prnt2 = SingletonExample.GetInstance;
+            prnt2.PrintDetails("Test2");
+        }
+
+        private static void Test1Prnt()
+        {
+            SingletonExample prnt1 = SingletonExample.GetInstance;
+            prnt1.PrintDetails("Test1");
         }
     }
 }
---
+++ DesignPatterns/Singleton/SingletonExample.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Singleton
+{
+    public sealed class SingletonExample
+    {
+        //Lazy loading
+        private static readonly Lazy<SingletonExample> instance = new Lazy<SingletonExample>(() => new SingletonExample());
+        //private static readonly object lockInstance = new object();
+        public static SingletonExample GetInstance
+        {
+            get
+            {
+            //    if (instance == null)
+            //    {
+            //        lock (lockInstance)
+            //        {
+            //            if (instance == null)
+            //                instance = new SingletonExample();
+            //        }
+            //    }
+                return instance.Value;
+            }
+        }
+        private static int counter = 0;
+        private SingletonExample()
+        {
+            counter++;
+            Console.WriteLine($"Instance Count:{counter}");
+        }
+
+        public void PrintDetails(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
